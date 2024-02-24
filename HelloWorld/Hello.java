83e07a7d15c8c187f9c337915e042e217407d406
Githuba Jeremy <githuba9520@gmail.com>
2024-02-24 13:10:03 +0300

    variables: numbers(int)

--- HelloWorld/Hello.java
+++ HelloWorld/Hello.java
@@ -1,7 +1,16 @@
-class Hello { //since java is Object Oriented everythingneeds to be in a class //
+class Hello { // since java is Object Oriented everythingneeds to be in a class //
 
-    public static void main(String a[]) { //this is the signature to identify main file required by jvm //
-        System.out.print("Hello world");
+    public static void main(String a[]) { // this is the signature to identify main file required by jvm //
+        System.out.println("Hello world"); // semicolon at the end of each line to identify the end of the line
 
-    }
+        System.out.println(3 + 2); // println the ln means new line, it prints the arhumrnt provided and kips the
+                                   // cursor to the new line //
+        System.out.println(23 + 45);
+
+        // java is a strongly typed language so you have to declare types of variables
+        // when assigning //
+        int num = 3;
+        System.out.println(num);
+
+    } // this signifies the end of a block and it does not need a semicolon
 }
