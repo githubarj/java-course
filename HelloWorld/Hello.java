58f6d31f7addf2aabdfd3cd526cacb50cabcc932
Githuba Jeremy <githuba9520@gmail.com>
2024-02-24 12:44:01 +0300

    initial commit

---
+++ HelloWorld/Hello.java
@@ -0,0 +1,7 @@
+class Hello { //since java is Object Oriented everythingneeds to be in a class //
+
+    public static void main(String a[]) { //this is the signature to identify main file required by jvm //
+        System.out.print("Hello world");
+
+    }
+}
