209e3991f143fbb8a55124429ccd999221706f0e
Githuba Jeremy <githuba9520@gmail.com>
2024-02-25 22:07:29 +0300

    day2

--- HelloWorld/Conditional.java
+++ HelloWorld/Conditional.java
@@ -2,13 +2,32 @@ class Conditional {
     public static void main(String a[]) {
 
         int x = 2;
+        int y = 4;
+        int t = 5;
 
-        if (x > 5)
+        if (x > y && x > t ){
+            System.out.println(x);
 
-        {
-            } else {
-            System.out.println("Bye");
         }
+        else if (y > t) 
+        {
+            System.out.println(y);
+            
+        } else {
+            System.out.println(t);
+        }
+
+        int result = x % 2 == 0 ? 10 : 20;
+
+        // if(x % 2 == 0) { //checking for an even or oddnumber 
+        //     result = 10;
+        // } else {
+        //     result = 20;
+        // }
+
+        System.out.println(result);
+
+       
 
     }
 }
