cde453517927fa6a024a6b2041515ebac4ba2c9d
Githuba Jeremy <githuba9520@gmail.com>
2024-02-25 20:56:22 +0300

    conditional statements

---
+++ HelloWorld/Conditional.java
@@ -0,0 +1,14 @@
+class Conditional {
+    public static void main(String a[]) {
+
+        int x = 2;
+
+        if (x > 5)
+
+        {
+            } else {
+            System.out.println("Bye");
+        }
+
+    }
+}
