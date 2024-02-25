b271d7cdd97ac45a038084c03f541c36d40a7a9a
Githuba Jeremy <githuba9520@gmail.com>
2024-02-25 20:26:42 +0300

    compare

---
+++ HelloWorld/compare.java
@@ -0,0 +1,17 @@
+class compare {
+    public static void main (String a []){
+
+        int x = 6;
+        int y = 6;
+        boolean result = x == y;
+
+        double r = 45.7777;
+        double b = 23.29181;
+
+        result = r > b;
+
+        System.out.println(result);
+
+
+    }
+}
