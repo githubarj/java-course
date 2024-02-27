07d46e5806a964e7f68355e876ff1935e4182356
Githuba Jeremy <githuba9520@gmail.com>
2024-02-27 14:51:12 +0300

    arrays

---
+++ OOP/arrays.java
@@ -0,0 +1,15 @@
+class Arrays {
+    public static void main (String a[]){
+        int[] nums = {3,5,6,7};
+        nums[3] = 23;
+
+        for (int i=0; i < nums.length; i++){
+            System.out.println(nums[i]);
+        }
+
+        int[] arr = new int[5];
+
+       
+        
+    }
+}
