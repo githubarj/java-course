4a51556bbbc09f35131af93eaa7d5d01ca08ef46
Githuba Jeremy <githuba9520@gmail.com>
2024-02-28 11:00:47 +0300

    strings and string manipulation

---
+++ OOP/Demo2.java
@@ -0,0 +1,11 @@
+ class Demo2 {
+    public static void main (String a[]){
+        String name = "Richard";
+        System.out.println(name);
+
+        StringBuffer name2 = new StringBuffer("Githuba");
+        name2.append(" is ready");
+        System.out.println(name2);
+
+    }
+}
