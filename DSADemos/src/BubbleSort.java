be85ba08f5873b1047d48e8847bf62e60bb44331
Jeremy Richard <githubarj@gmail.com>
2025-04-25 21:05:16 +0100

    bubble sort

--- DSADemos/src/BubbleSort.java
+++ DSADemos/src/BubbleSort.java
@@ -10,7 +10,7 @@ public class BubbleSort {
 
     public static int[] bubbleSort (int[] nums) {
         for (int j = nums.length - 1; j > 0 ; j--){
-            for (int i  = 0; i < j; i++ ) {
+            for (int i  = 0; i < j ; i++ ) {
                 if(nums[i] > nums[i+1]){
                     int temp = nums[i];
                     nums[i] = nums[i+1];
