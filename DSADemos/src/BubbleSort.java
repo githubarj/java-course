63c0fffb480cf59f687bc888077fa0285e54ec28
Jeremy Richard <githubarj@gmail.com>
2025-04-25 16:19:59 +0100

    bubblesort mine

---
+++ DSADemos/src/BubbleSort.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+public class BubbleSort {
+    public static void main (String[] args) {
+        int[] nums = {5, 3, 9, 7, 12, 26, 1, 10, 2};
+        int[] result = bubbleSort(nums);
+        System.out.println("The sorted array is: " +  Arrays.toString(result));
+
+    }
+
+    public static int[] bubbleSort (int[] nums) {
+        for (int j = nums.length - 1; j > 0 ; j--){
+            for (int i  = 0; i < j; i++ ) {
+                if(nums[i] > nums[i+1]){
+                    int temp = nums[i];
+                    nums[i] = nums[i+1];
+                    nums[i + 1] = temp;
+                }
+            }
+        }
+        return nums;
+    }
+}
