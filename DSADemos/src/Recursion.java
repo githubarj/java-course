997164e9aaf2b5e56f762344922f72b88d5e07bc
Jeremy Richard <githubarj@gmail.com>
2025-04-30 09:26:18 +0100

    factorial using recursion

--- DSADemos/src/Recursion.java
+++ DSADemos/src/Recursion.java
@@ -1,6 +1,7 @@
 public class Recursion {
     public static void main(String[] args){
             f1(0);
+            System.out.println("The factorial is :" + factorial(5));
     }
 
     public static void f1(int i){
@@ -11,5 +12,12 @@ public class Recursion {
             f1(i);
         }
 
+    }
+
+    public static int factorial(int n){
+        if ( n !=  0 ) {
+            return n * factorial(n - 1);
+        }
+       return 1;
     }
 }
