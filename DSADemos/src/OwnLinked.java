d30e4692246f08ab6b284b66c7e0dfeb9da1c0cd
Jeremy Richard <githubarj@gmail.com>
2025-04-30 16:45:09 +0100

    corrected error in deletion

--- DSADemos/src/OwnLinked.java
+++ DSADemos/src/OwnLinked.java
@@ -7,13 +7,13 @@ public class OwnLinked {
         Node next; // since it must point to a node
 
         // constructor for the node
-        public Node(int data){
+        public Node(int data) {
             this.data = data;
             next = null; // initialize the node with the next address as null since it is last in
         }
     }
 
-    public static class LinkedList{
+    public static class LinkedList {
 
         Node head = null; // create head node
 
@@ -31,69 +31,73 @@ public class OwnLinked {
             }
         }
 
-        public void printValues (){
+        public void printValues() {
             Node current = head;
 
-            while(current != null){
+            while (current != null) {
                 System.out.print(current.data + " ");
                 current = current.next;
             }
         }
 
-        public void addBegining(int data){
+        public void addBegining(int data) {
             Node newNode = new Node(data);
             newNode.next = head;
             head = newNode;
         }
 
-        public void pop(){
+        public void pop() {
             Node current = head;
-            while(current.next.next != null){
+            while (current.next.next != null) {
                 current = current.next;
             }
             current.next = null;
         }
 
-        public void delete(int data){
-          // in case of an empty list
-            if(head == null) {
+        public void delete(int data) {
+            // in case of an empty list
+            if (head == null) {
                 System.out.println("List is empty");
-
-                // handle deleting head node
-                if (head.data == data){
-                    head = head.next; // just set the head to the next value after current head
-                    return;
-                }
-
-                Node current = head;
-                Node prev = null; // keep track of previous node
-
-                while (current != null && current.data != data){
-                    prev = current; // keep track
-                    current = current.next;  // change to next
-                }
-
-                // if the value does not exist
-                if (current == null){
-                    System.out.println("Node with data " + data + " not found");
-                    return;
-                }
-
-                prev.next = current.next;
+                return;
             }
+
+
+            // handle deleting head node
+            if (head.data == data) {
+                head = head.next; // just set the head to the next value after current head
+                return;
+            }
+            Node current = head;
+            Node prev = null; // keep track of previous node
+
+            while (current != null && current.data != data) {
+                prev = current; // keep track
+                current = current.next;  // change to next
+            }
+
+            // if the value does not exist
+            if (current == null) {
+                System.out.println("Node with data " + data + " not found");
+                return;
+            }
+
+            prev.next = current.next;
         }
+
     }
 
     // the node is garbage collected by the JVM because it had no node referencing it
 
-    public static void main (String[] args){
+    public static void main(String[] args) {
         LinkedList nums = new LinkedList();
 
         nums.add(5);
         nums.add(9);
         nums.add(10);
+        nums.add(11);
         nums.addBegining(120);
         nums.pop();
+        nums.delete(9);
 
         nums.printValues();
     }
