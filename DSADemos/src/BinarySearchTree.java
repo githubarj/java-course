26f08cb48441f2866bd761fd87af514eae9c50a4
Jeremy Richard <githubarj@gmail.com>
2025-05-02 09:00:51 +0100

    binary search tree functions

--- DSADemos/src/BinarySearchTree.java
+++ DSADemos/src/BinarySearchTree.java
@@ -35,6 +35,24 @@ public class BinarySearchTree {
     }
 
     public static void main(String[] args) {
+        Tree bst = new Tree();
 
+        bst.add(10);
+        bst.add(5);
+        bst.add(15);
+        bst.add(3);
+        bst.add(7);
+
+        // Output the tree structure (in-order traversal)
+        printInOrder(bst.root);
+    }
+
+    // Helper function for in-order traversal
+    public static void printInOrder(Node node) {
+        if (node != null) {
+            printInOrder(node.childLeft);
+            System.out.print(node.data + " ");
+            printInOrder(node.childRight);
+        }
     }
 }
